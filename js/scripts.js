2141e6e0c138a0e21c9d9b637989506f8acb01b9
stiva98 <samustiva9810@gmail.com>
2023-06-27 18:28:11 +0200

    ADD milestone 1 - 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,69 @@
 const btnUp = document.getElementById('btnUp');
 const btnDown = document.getElementById('btnDown');
-let activeImg = 1;
+let activeImg = 0;
+
+const imgArr = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    },
+];
+//console.log(imgArr)
 
 
 btnUp.addEventListener('click', function(){
     
-    if(activeImg < 5){
+    if(activeImg < 4){
         activeImg++;
     }else{
-        activeImg = 1;
+        activeImg = 0;
     }   
-    const newImage = '<img src="img/0'+activeImg+'.webp" alt="">';   
+
     const imgOn = document.querySelector('.img-on');
-    imgOn.innerHTML = newImage;
+    const newImageAndText = 
+    `<img src="${imgArr[activeImg].image}" alt="">
+    <div class="text">
+    <h2>${imgArr[activeImg].title}</h2>
+    <p>${imgArr[activeImg].text}</p>
+    </div>`; 
+    
+    imgOn.innerHTML = newImageAndText;
+    
+
 });
 
 btnDown.addEventListener('click', function(){
     
-    if(activeImg <= 5 && activeImg > 1){
+    if(activeImg > 0){
         activeImg--;
     }else{
-        activeImg = 5;
+        activeImg = 4;
     }   
-    const newImage = '<img src="img/0'+activeImg+'.webp" alt="">';   
     const imgOn = document.querySelector('.img-on');
-    imgOn.innerHTML = newImage;
+    const newImageAndText = 
+    `<img src="${imgArr[activeImg].image}" alt="">
+    <div class="text">
+    <h2>${imgArr[activeImg].title}</h2>
+    <p>${imgArr[activeImg].text}</p>
+    </div>`; 
+    
+    imgOn.innerHTML = newImageAndText;
+
 });
